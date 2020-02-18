6bee762fa784f0c0d2424463a57bd7f46743f62b
Marcelo Martins <m.a.martins@live.com>
2020-02-17 22:44:55 -0300

    Added dynamic content

---
+++ src/components/LaunchList/LaunchList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { LaunchListQuery } from "../../generated/graphql";
+
+import "./styles.css";
+
+type Props = {
+  data: LaunchListQuery
+}
+
+const className = "LaunchList";
+
+const LaunchList: React.FC<Props> = ({ data }) => (
+  <div className={className}>
+    <h3>Launches</h3>
+    <ol className={`${className}__list`}>
+      {!!data.launches && data.launches.map((launch, i) =>
+        !!launch && (
+          <li key={i} className={`${className}__item`}>
+            {launch.mission_name} ({launch.launch_year})
+          </li>
+        )
+      )}
+    </ol>
+  </div>
+);
+export default LaunchList
